aec9d7fd5a0c10873c942335bbe0f7a1afb8da3a
David <daviddong201712@gmail.com>
2020-05-07 12:01:26 -0500

    add some tests

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,6 +12,37 @@ public class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void onlyLeftOneBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void onlyRightOneBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void stringBeforeBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("I Love you.[]"));
+    }
+    @Test
+    public void stringBehindBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]I Love you."));
+    }
+    @Test
+    public void stringMixBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("I Love you[Miley]."));
+    }
+    @Test
+    public void stringMixTwoBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("I [Love] you[Miley]."));
+    }
+
 
 
 }
